1bec9bc5407df12bd3cd422414f14c2300c2daae
Junayed5 <mdjunayed352003@gmail.com>
2022-06-30 21:20:07 +0600

    set route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './component/Header';
+import { Routes, Route} from "react-router-dom";
+import Tasks from './component/Tasks';
+import ToDo from './component/ToDo';
+import Calender from './component/Calender';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Routes>
+        <Route path="/completeTask" element={<Tasks/>}/>
+        <Route path='/toDo' element={<ToDo/>}/>
+        <Route path='/calender' element={<Calender/>}/>
+      </Routes>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,19 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        taskTheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#3A4256",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+
 }
